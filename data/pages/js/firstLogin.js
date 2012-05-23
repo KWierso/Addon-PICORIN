110804e6791c1e1c92c0e3a2debac0c5136246e2
Withered Flowers <provemeifimright@gmail.com>
2012-05-23 12:42:12 +0700

    Updated to v0.04

--- data/pages/js/firstLogin.js
+++ data/pages/js/firstLogin.js
@@ -1,15 +1,34 @@
-$("#dialog").dialog({
-	closeOnEscape:false,
-	draggable: false,
-	modal:true,
-	minHeight: 250,
-	minWidth: 450,
-	buttons: {
-		"Save Password": function() {
-			self.port.emit("storeUserInformation", $("input:[name=uname]").val(), $("input:[name=upass1]").val());
+$("#dialog")
+	.dialog({
+		closeOnEscape:false,
+		draggable: true,
+		resizeable: false,
+		modal:true,
+		minHeight: 250,
+		minWidth: 450,
+		buttons: {
+			"Save Password": function() {
+				try {
+					if( $("input:[name=upass1]").val() != $("input:[name=upass2]").val())
+						throw "Password value is not same !";
+					else if( $("input:[name=upass1]").val().length < 6 )
+						throw "Password must be 6 digits or higher !";
+				
+					self.port.emit("storeUserInformation", $("input:[name=upass1]").val());
+				}
+				catch(err) {
+					alert(err);
+				}
+			}
+		},
+		open: function() { 
+			$(".ui-dialog-titlebar-close").hide(); 
 		}
-	},
-	open: function() { 
-		$(".ui-dialog-titlebar-close").hide(); 
-	}
-})
+	})
+	
+self.port.on("successStoreInformation", function() {
+	alert("Thank you for setting the password !");
+	$("#dialog").dialog("close");
+	
+	self.port.emit("blankPage");
+});
